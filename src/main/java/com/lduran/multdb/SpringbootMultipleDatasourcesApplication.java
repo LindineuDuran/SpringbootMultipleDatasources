d5e52919a1a4831b357e8946f96762fdd91c8b1e
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2019-11-09 15:02:24 -0300

    Initial commit

---
+++ src/main/java/com/lduran/multdb/SpringbootMultipleDatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.lduran.multdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultipleDatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMultipleDatasourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lduran/multdb/SpringbootMultipleDatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lduran.multdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultipleDatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
